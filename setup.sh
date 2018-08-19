3e53c86fc87ab20d8afd0ef39ef1708edba1a77f
Julien Parent-Trudeau <julien.parent-trudeau@sap.com>
2018-08-19 17:29:35 -0400

    Add mappings

--- setup.sh
+++ setup.sh
@@ -3,20 +3,54 @@
 elasticuser="$1"
 elasticpass="$2"
 
-echo "=== Updating logstash_system user's password ================"
+echo "=== Updating logstash_system user's password"
 # We need to setup the logstash_system user's password once the cluster has been created
 # For demo purposes, we'll use the same password as the elasticsearch password
 curl --user "${elasticuser}:${elasticpass}" -XPUT -H 'Content-Type: application/json' 'localhost:9200/_xpack/security/user/logstash_system/_password' -d\
 '{
   "password": "'"${elasticpass}"'"
 }'
-echo "\n=== Done ====================================================\n"
+echo "\n=== Done\n"
 
-echo "=== Uncompressing data files ================================"
+echo "=== Uncompressing data files"
 # Uncompressed the test data files
 unzip -o data/accounts.zip -d data
 gunzip -f -k data/logs.jsonl.gz
-echo "=== Done ====================================================\n"
+echo "=== Done\n"
 
 
+echo "=== Creating mappings"
+curl --user "${elasticuser}:${elasticpass}" -XPUT -H 'Content-Type: application/json' 'localhost:9200/shakespeare' -d\
+'{
+  "mappings": {
+  "doc": {
+   "properties": {
+    "speaker": {"type": "keyword"},
+    "play_name": {"type": "keyword"},
+    "line_id": {"type": "integer"},
+    "speech_number": {"type": "integer"}
+   }
+  }
+ }
+}' && echo ""
 
+for i in 18 19 20
+do
+curl --user "${elasticuser}:${elasticpass}" -XPUT -H 'Content-Type: application/json' "localhost:9200/logstash-2015.05.$i" -d\
+'{
+  "mappings": {
+    "log": {
+      "properties": {
+        "geo": {
+          "properties": {
+            "coordinates": {
+              "type": "geo_point"
+            }
+          }
+        }
+      }
+    }
+  }
+}' && echo ""
+done
+echo "=== Done\n"
